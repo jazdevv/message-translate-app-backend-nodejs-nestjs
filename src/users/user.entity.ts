5c4563577e7bcf95e04cf34cdc3a2f8b89f42a41
Jazthedev <joelpepa2@gmail.com>
2023-02-03 16:51:11 +0100

    add users entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,7 +1,23 @@
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
+
 @Entity()
 export class User{
     @PrimaryGeneratedColumn()
     id: number;
+
+    @Column({unique: true})
+    nametag: string;
+
+    @Column({unique: true})
+    email: string
+
+    @Column()
+    profileImage: string;
+
+    @Column()
+    password: string
+
+    @Column({default: true})
+    isGoogleUser: boolean
 }
