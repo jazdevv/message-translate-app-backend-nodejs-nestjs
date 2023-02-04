a2da48f46d200b4b06bd9d60adc378dfb154f810
Jazthedev <joelpepa2@gmail.com>
2023-02-04 21:10:29 +0100

    add template engine handlebars

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,21 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import * as hbs from 'express-handlebars';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
   app.useGlobalPipes(new ValidationPipe({whitelist: true, transform: true}));
+  
+  //PUBLIC FOLDERS FOR TEMPLATE ENGINE
+  app.useStaticAssets(join(__dirname,'..',  '/public'))  
+  // SET TEMPLATE ENGINE
+  app.setBaseViewsDir(join(__dirname,'..', '/views'));
+  app.engine('hbs', hbs({ extname: 'hbs' }));
+  app.setViewEngine('hbs');
   await app.listen(3000);
 }
 bootstrap();
