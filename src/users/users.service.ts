60b6e512852d003f48c6b9f5c28a934f14c918d0
Jazthedev <joelpepa2@gmail.com>
2023-02-04 11:01:50 +0100

    hash users passwords

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
 export class UsersService {
@@ -14,20 +15,26 @@ export class UsersService {
         if(EmailExists.length > 0){throw new BadRequestException("email  already in use")}
         const UserNameExists = await this.repo.find({where:{username:username}})
         if(UserNameExists.length  > 0 ){ throw new BadRequestException("name  already in use")}
-
+        //HASH THE PASSWORD
+        const salt = await bcrypt.genSalt();
+        const hashedpassword = await bcrypt.hash(password,salt)
         //CREATE THE USER
-        const user = this.repo.create({email,username,password,isGoogleUser})
+        const user = this.repo.create({email,username,password:hashedpassword,isGoogleUser})
         this.repo.save(user);
 
         return user
     }
 
     async loginUser(email: string, password: string){
-        
-        //VERIFY USER EXISTS & PASSWORDS MATCHES
+
+        //GET USER
         const user =  await this.repo.findOne({where:{email:email}})
+        //THROW ERROR IF USER WITH THAT EMAIL DONT EXISTS
         if(!user){throw new BadRequestException("email not registred with that password")}
-        if(user.password != password){throw new BadRequestException("password incorrect")}
+        //COMPARE INPUT PASSWORD WITH THE DATABASE USER HASHED PASSWORD //match = true , dont match = false
+        const passwordMatches: boolean = await bcrypt.compare(password,user.password)
+        //IF PASSOWRD DONT MATCHES THROW AN ERROR
+        if(passwordMatches === false){throw new BadRequestException("password incorrect")}
 
         return user
     }
