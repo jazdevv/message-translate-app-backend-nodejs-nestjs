a85200e903ea8944c179e96ed96088e8c29d6202
Jazthedev <joelpepa2@gmail.com>
2023-02-06 19:28:10 +0100

    add rooms service

--- src/messages/rooms.service.ts
+++ src/messages/rooms.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Room } from './rooms.entity';
 
 @Injectable()
-export class RoomsService {}
+export class RoomsService {
+    constructor(@InjectRepository(Room) private repo: Repository<Room>){}
+    
+    async createOrGetRoom(id1:number,id2:number){
+        //`SELECT * FROM room WHERE users @> '[{"userid":3}]'::jsonb and users @> '[{"userid":1}]'::jsonb   `
+        const sqlquery = `SELECT roomid FROM room WHERE users @> '[{"userid":` + id1 + `}]'::jsonb and users @> '[{"userid":` + id2 + `}]'::jsonb `
+        let roomid = await this.repo.query(sqlquery);
+
+         if(roomid.length === 0){
+            let roomid = await this.repo.create({roomid:"877bjnbypkpokokp",users:[{userid:id1,userhavenotis:false},{userid:id2,userhavenotis:false}]})
+            this.repo.save(roomid);
+            console.log(roomid)
+            console.log("---")
+            
+         }
+       
+        return roomid
+    }
+
+    
+}
