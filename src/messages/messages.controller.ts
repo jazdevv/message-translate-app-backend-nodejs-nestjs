02cbbc375c285129b221905f1d224968ba14f351
Jazthedev <joelpepa2@gmail.com>
2023-02-06 19:32:03 +0100

    add room frontend controller api + view

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { RoomsService } from './rooms.service';
 
-@Controller('messages')
-export class MessagesController {}
+@Controller('')
+export class MessagesController {
+    constructor(private repoRooms: RoomsService){}
+
+    @Get('/chat/:username')
+    chooseChat(@Res() res: Response){
+        //temporary till implement the frontend logic
+        res.cookie("acces_token","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOjksImlhdCI6MTY3NTU0ODcyMiwiZXhwIjoxNjgzMzI0NzIyfQ.Wl6tia-ePCKrsdjZH_9hps69n9U6bTlYgsIIA7tfrfY")
+        //render the template
+        return res.render('chat',{layout:"chat"});
+
+    }
+
+    @Get('/chats')
+    userChat(){
+
+    }
+
+}
+
