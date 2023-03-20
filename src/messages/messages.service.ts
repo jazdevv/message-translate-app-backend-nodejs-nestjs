f565db0c6f3886715d50b5559f051331ed2570de
Jazthedev <joelpepa2@gmail.com>
2023-03-20 11:59:34 +0100

    add messages service - create message

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/users/user.entity';
+import { Repository } from 'typeorm';
+import { Message } from './messages.entity';
+import { RoomsService } from './rooms.service';
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+    constructor(@InjectRepository(Message) private repo: Repository<Message>, private readonly repoRooms:RoomsService){}
+
+    async createMessage(createdBy: User, text: string, image: any, roomid: number){
+        //determine the data types of the message
+        let type = [];
+        let image_uuid: string = null;
+        if(image){
+            //generate an image uuid
+            type.push("image")
+        }
+        if(text){
+            image_uuid  = "message/image/" + uuidv4() + "_date_" + Date.now();
+            type.push("text")
+        }
+        const message = await this.repo.create({UserSender:createdBy.id,text,roomid,type,imageUrl:image_uuid});
+        this.repo.save(message);
+        
+        return message
+    }
+
+    getMessages(){
+        
+    }
+}
