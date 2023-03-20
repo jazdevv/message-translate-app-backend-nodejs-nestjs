373afb2d798b89defcfba18432f350bc291a16eb
Jazthedev <joelpepa2@gmail.com>
2023-03-20 11:58:52 +0100

    add messages entity

--- src/messages/messages.entity.ts
+++ src/messages/messages.entity.ts
@@ -1,7 +1,23 @@
-import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-
+import { Entity, Column, PrimaryGeneratedColumn, OneToOne } from 'typeorm';
+import { User } from 'src/users/user.entity';
+import { Room } from './rooms.entity';
 @Entity()
 export class Message{
     @PrimaryGeneratedColumn()
     id: number;
+
+    @OneToOne(()=>User)
+    UserSender: number
+
+    @OneToOne(()=>Room)
+    roomid: number
+
+    @Column({default:null})
+    text: string;
+
+    @Column({default:null})
+    imageUrl: string
+
+    @Column('character varying',{array:true})
+    type: string[];
 }
